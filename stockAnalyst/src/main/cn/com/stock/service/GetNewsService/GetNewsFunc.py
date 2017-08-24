7fa5f693a86232d9435ccc1cdc6d17651a30e9a9
Ciel-247 <ciel_247@hotmail.com>
2017-08-24 20:57:55 +0800

    getnewsfunc update

--- stockAnalyst/src/main/cn/com/stock/service/GetNewsService/GetNewsFunc.py
+++ stockAnalyst/src/main/cn/com/stock/service/GetNewsService/GetNewsFunc.py
@@ -3,7 +3,23 @@ import tushare as ts
 
 
 # 需要定义一个基类里面放公共的方法，比如获取当前要查询的股票代码，并且支持多个
-STOCKCODE = 000002
-newscode = STOCKCODE
 
 
+
+class GetNewsFunc(object):
+    def __init__(self,dateS,dateE):
+        # self.stockcodes = stockcodes
+        self.dateS = dateS
+        self.dateE = dateE
+    def get_latest_news(self,topN):
+        latest_news = ts.get_latest_news(topN)
+        return latest_news
+    def get_notices(self,code):
+        notices = ts.get_notices(code,self.dateS)
+        return notices
+
+
+this = GetNewsFunc('2017-08-25','2017-08-24')
+print this.get_notices('000002')
+
+# print ts.get_notices('000002','2017-08-11')
